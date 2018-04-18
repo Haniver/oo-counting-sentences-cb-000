46c0e06e783749f99ba08066f6163c6c0388b5a2
Lucio Ernesto Gutierrez Munch <luciogm@gmail.com>
2018-04-18 17:39:46 +0000

    automatically backed up by learn

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -4,18 +4,20 @@ class String
 
   def sentence?
     return true if self.end_with?(".")
-    return false
+    false
   end
 
   def question?
-
+    return true if self.end_with?("?")
+    false
   end
 
   def exclamation?
-
+    return true if self.end_with?("!")
+    false
   end
 
   def count_sentences
-
+    #self.split(/!?./)
   end
 end
